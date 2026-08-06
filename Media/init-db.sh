9db51d4d4a671e150ba687cafa708da05e7829dc
Joakim Hellsén <tlovinator@gmail.com>
2026-08-06 20:25:09 +0200

    Add PostgreSQL configuration and initialization scripts for Sonarr and Radarr

---
+++ Media/init-db.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    -- Create Users
+    CREATE USER sonarr WITH PASSWORD '$POSTGRES_SONARR_PASSWORD';
+    CREATE USER radarr WITH PASSWORD '$POSTGRES_RADARR_PASSWORD';
+
+    -- Create Backup User (Global Read-Only access)
+    CREATE USER backup WITH PASSWORD '$POSTGRES_BACKUP_PASSWORD';
+    GRANT pg_read_all_data TO backup;
+
+    -- Create Sonarr Databases
+    CREATE DATABASE sonarr_default_main OWNER sonarr;
+    CREATE DATABASE sonarr_default_log OWNER sonarr;
+    CREATE DATABASE sonarr_anime_main OWNER sonarr;
+    CREATE DATABASE sonarr_anime_log OWNER sonarr;
+
+    -- Create Radarr Databases
+    CREATE DATABASE radarr_default_main OWNER radarr;
+    CREATE DATABASE radarr_default_log OWNER radarr;
+    CREATE DATABASE radarr_anime_main OWNER radarr;
+    CREATE DATABASE radarr_anime_log OWNER radarr;
+EOSQL
